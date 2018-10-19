a523dcf6feb0784e692fe4cce8186eaf2bb33e55
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-10-19 00:18:04 -0400

    PWA'd it aw yeah

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+      <label htmlFor="search"> Search </label>
       <input
+        id="search"
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
