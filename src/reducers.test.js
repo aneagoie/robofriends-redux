14b8a88bce649eb7d20b8cbec5b7bd9d97b243ed
Stephen Axtell <axtell.stephen@gmail.com>
2018-12-03 13:29:30 +0200

    adding tests for actions and reducers

---
+++ src/reducers.test.js
@@ -0,0 +1,77 @@
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants';
+
+import * as reducers from './reducers';
+
+describe('searchRobots', () => {
+
+  const initialStateSearch = {
+    searchField: ''
+  };
+
+  it('should return the initial state', () => {
+    expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: ''});
+  });
+
+  it('should handle CHANGE_SEARCHFIELD', () => {
+    expect(reducers.searchRobots(initialStateSearch, {
+      type: CHANGE_SEARCHFIELD,
+      payload: 'xyz'
+    })).toEqual({
+      searchField: 'xyz'
+    });
+  });
+});
+
+describe('requestRobots', () => {
+  const initialStateRobots = {
+    robots: [],
+    isPending: false
+  };
+
+  it('should return the initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+  });
+
+  it('should handle REQUEST_ROBOTS_PENDING action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_PENDING
+    })).toEqual({
+      robots: [],
+      isPending: true
+    });
+  });
+
+  it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_SUCCESS,
+      payload: [{
+        id: '123',
+        name: 'test',
+        email: 'test@test.com'
+      }]
+    })).toEqual({
+      robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@test.com'
+      }],
+      isPending: false
+    });
+  });
+
+  it('should handle REQUEST_ROBOTS_FAILED action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_FAILED,
+      payload: 'Error happened'
+    })).toEqual({
+      error: 'Error happened',
+      robots: [],
+      isPending: false
+    });
+  });
+});
