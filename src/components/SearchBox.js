6c4a046642550d54dca1758ac3f0db14735d38a5
Subin Varghese <subinvarghese99@hotmail.com>
2019-06-25 07:08:29 +0530

    commited

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './SearchBox.css'
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+    <h3><u><label htmlFor="r_search">Search Robots</label></u></h3>
       <input
+        id='r_search'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
