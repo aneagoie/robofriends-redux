fe7dcf5c21b065f34a9e523934d60f982c2a3040
Subin Varghese <subinvarghese99@hotmail.com>
2019-06-25 11:52:08 +0530

    commited

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import './SearchBox.css'
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-    {/* <h3><u><label htmlFor="r_search">Search Robots</label></u></h3> */}
       <input
         aria-label='Search Robots'
         className='pa3 ba b--green bg-lightest-blue'
