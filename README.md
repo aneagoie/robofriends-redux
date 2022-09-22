06221f166251667b0d9f8a55e6ac649efb3dd62b
Andrei Neagoie <aneagoie@Andreis-MBP-2.lan>
2022-09-22 13:00:24 -0400

    update

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ To run the project:
 2. Run `npm install`
 3. Run `npm start`
 
+*visist https://zerotomastery.io/ for more*
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
@@ -19,10 +20,17 @@ const rootReducers = combineReducers({requestRobots, searchRobots})
 
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+root.render(
+<Provider store={store}>
+  <App/>
+</Provider>
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 registerServiceWorker();
